8d16b5fdd15a59a8bdd7d44d18e0285e03602908
Leo is awesome <lc599@drexel.edu>
2015-02-26 03:28:06 -0500

    still need to work on getting the thing to not bounce when posting new comment

--- client/client.js
+++ client/client.js
@@ -6,9 +6,9 @@ Template.chatrow.helpers({
     messages: function() {
         var messages = Messages.find({}, { sort: { time: 1}});
         var messagesArray = messages.fetch();
-        if (messagesArray.length > 100){
+        if (messagesArray.length > 50){
             // just for development, limit the size of the collection
-            for (var i = 0; i < 50; i++){
+            for (var i = 0; i < 25; i++){
                 Messages.remove(messagesArray[i]["_id"]);
             }
         }
@@ -83,7 +83,6 @@ function hidingFunction(index, that){
     }
     else {
         $(that).css({
-            "that": "1",
             "font-size": "15px"
         });
         $(that).find(".bubble").css({
@@ -96,54 +95,122 @@ function hidingFunction2(index, that){
     // Track distance between each initial top position and
     // original center of screen. If the distance is greater than
     // a specified fraction of the screen height, make it disappear
-    var topDist = $(that).position().top; // distance from top of chat row top top of screen
-    var botDist = ph+pt-topDist; // distance from top of chat row to bottom of screen
+    var midDist = Math.abs($(that).position().top - mid);
 
-
-    if (topDist < limit){
-        $(that).css({
-            "font-size": "1.5px"
-        });
-        $(that).find(".bubble").css({
-            "width": "10%"
-        });
+    if (midDist > limit+buffer){
+        changeRow(that);
     }
-    else if (botDist < limit+bottomBuffer){
-        $(that).css({
-            "font-size": "1.5px"
-        });
-        $(that).find(".bubble").css({
-            "width": "10%"
-        });
+    else if (midDist < buffer) {
+        changeRow(that,maxProperties);
     }
     else {
-        $(that).css({
-            "that": "1",
-            "font-size": "15px"
-        });
-        $(that).find(".bubble").css({
-            "width": w + "%"
-        });
+        /*
+        # NOTES
+        font size: (15-1.5)/(0-limit)*midDist + 15
+        bubbleWidth: (100-10)/(0-limit)*midDist + 100
+        margin-left: (5-15)/(0-limit)*midDist + 5
+        margin-right: (5-15)/(0-limit)*midDist + 5
+        avatar: (5-2)/(0-limit)*midDist + 5
+
+        15 = -14.5/limit*buffer + yIndex
+        yIndex = 15 + 14.5/limit*buffer
+        font-size = 15 + 14.5/limit*buffer + 14.5/limit*(-midDist)
+                  = 14.5/limit*(buffer-midDist) + 15
+
+        property = (max-default)/limit*(midDist-buffer) + max
+        */
+        var properties = {
+            "font-size": (-14.5/limit*(midDist-buffer) + 15) + "px",
+            "bubbleWidth": (-90/limit*(midDist-buffer) + 100) + "%",
+            "margin-left": (10/limit*(midDist-buffer) + 5) + "%",
+            "margin-right": (10/limit*(midDist-buffer) + 5) + "%",
+            "avatarWidth": (-3/limit*(midDist-buffer) + 5) + "%"
+        };
+        changeRow(that, properties);
     }
+
+    if (typeof $(that).data("color") === "undefined"){
+        var color = randElem(colors);
+        $(that).data("color", color);
+        $(that).find(".bubble").css("background-color", color.replace("n","")).addClass(color.replace("#",""));
+        //$(that).find(".avatar").css("background-color", color.replace("n","")).addClass(color.replace("#",""));
+    }
+
+}
+
+function changeRow(that, properties){
+    var nextProperties = $.extend({}, defaultProperties, properties);
+    $(that).css({
+        "font-size": nextProperties["font-size"],
+        "margin-left": nextProperties["margin-left"],
+        "margin-right": nextProperties["margin-right"]
+    });
+    $(that).find(".bubble").css({
+        "width": nextProperties["bubbleWidth"]
+    });
+    $(that).find(".avatar").css({
+        "width": nextProperties["avatarWidth"]
+    });
 }
 
 
 // Because botDist measures dist from top of elem to bottom,
 // add a buffer to allow for elems coming from bottom to be invisible more
-var bottomBuffer = 0;
+var bottomBuffer = 30; // decrease to shift the bump down; increase to move it up
+var buffer = 50;
 var w = 90;
-var pt, ph;
-var limit;
+var pt, ph, mid;
+var limit = 100.0;
+
+var maxProperties = {
+    "font-size": "15px",
+    "bubbleWidth": "100%",
+    "margin-left": "5%",
+    "margin-right": "5%",
+    "avatarWidth": "5%"
+};
+var defaultProperties = {
+    "font-size": "1.5px",
+    "bubbleWidth": "10%",
+    "margin-left": "15%",
+    "margin-right": "15%",
+    "avatarWidth": "2%"
+};
+
+// add n so can be used ass class
+var colors = [
+    "#bbffff", // light blue
+    "#b694da", // some purple
+    "#bde0b1", // light green
+    "#a0e1b4", // darker green
+    "#a63333", // reddish
+    "#d090c4", // pinkish
+    "#fffed9", // yellowish,
+    "#ffd39b", // lighty orange
+    "n#74bbfb", // darker blue
+];
+
+
+var name = 'Anonymous';
+
+
+function randElem(array){
+    return array[Math.floor(Math.random()*array.length)];
+}
 
 $(window).load(function(){
     pt = $(".chat").position().top; // parent top
     ph = $(".chat").height(); // parent height
+    mid = pt + ph/2 - bottomBuffer;
 
-    limit = 100.0;
-    //limit = ph/2 + pt;
+    $(".filler-row").height(mid + "px");
 
     $(".chat").scroll(function(){
-        hideAndSeek();
+        // Not running this when not necessary prevents the bouncing
+        //if ($(".chat-row").last().position().top - mid > buffer){
+            hideAndSeek();
+        //}
+        //console.log($(".chat-row").last().position().top, mid, $(".chat-row").last().position().top - mid, $(".chat")[0].scrollHeight, $(".chat")[0].scrollHeight-$(".chat-row").last().position().top-$(".chat-row").last().outerHeight());
     });
 
     hideAndSeek = function(){
@@ -153,11 +220,6 @@ $(window).load(function(){
     };
 
     post = function(){
-        if (Meteor.user())
-            var name = Meteor.user().profile.name;
-        else
-            var name = 'Anonymous';
-
         if ($("#message").val().trim() !== ""){
             Messages.insert({
                 name: name,
@@ -176,5 +238,19 @@ $(window).load(function(){
     $("#post").click(function(){
         post();
     });
+
+    $("#submit-username").click(function(){
+        var input = $("#selected-user-name").val().trim();
+        var isAlphaNumeric = !/[^a-zA-Z0-9]/.test(input);
+        if (input !== "" && isAlphaNumeric){
+            name = input;
+            $(".username").text(name);
+            $("#myModal").modal("hide");
+        }
+        else if (!isAlphaNumeric){
+            alert("Please enter alphanumeric characters only");
+        }
+    });
+    $('#myModal').modal();
 
 });
